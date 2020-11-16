3cb1d5b342da39ab733e0a506a79959bcca074f5
amal <a.ouassit17@gmail.com>
2020-11-16 18:52:37 +0100

    mod

--- ft_read.s
+++ ft_read.s
@@ -1,5 +1,4 @@
     global _ft_read
-<<<<<<< HEAD
     extern  ___error
     section .text 
 _ft_read:
@@ -14,14 +13,4 @@ error:
     mov [rax], dl
     mov rax, -1
     pop rbp
-    
     ret
-=======
-    section .text
-_ft_read:
-    mov rax, 0x02000003
-    syscall
-    ret
-    mov rax, 0x02000001
-    syscall
->>>>>>> 1ca60ef0b059aefe3cca2b5263cb66bc71bf757e
--- ft_write.s
+++ ft_write.s
@@ -1,13 +1,8 @@
-<<<<<<< HEAD
-   global _ft_write
-;    call extern ___error
-=======
     global _ft_write
->>>>>>> 1ca60ef0b059aefe3cca2b5263cb66bc71bf757e
+    call extern ___error
+    global _ft_write
     section .text
 _ft_write:
     mov rax, 0x02000004
     syscall
     ret
-    mov rax, 0x02000001
-    syscall
